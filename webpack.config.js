0e17da7548a9cac63fa32dd191b79848c762af99
Almaz <almazzzik74@yandex.ru>
2020-07-12 09:19:17 +0300

    learned loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,13 @@ module.exports = {
       template: './index.html'
     }),
     new CleanWebpackPlugin()
-    
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      }
+    ]
+  }
 }
